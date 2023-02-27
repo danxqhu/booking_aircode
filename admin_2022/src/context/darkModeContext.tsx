73d675daf0d96cd36bba87b46d5f4ce064b910ce
xinqiang-hu <danxqhu@gmail.com>
2023-02-27 17:30:39 +0800

    update ts bug

--- admin_2022/src/context/darkModeContext.tsx
+++ admin_2022/src/context/darkModeContext.tsx
@@ -5,15 +5,17 @@ const INITIAL_STATE = {
   darkMode: false,
 };
 
-type Dark = {
-  darkMode: boolean;
-  dispatch: any;
-};
+// type IDarkModeContext = {
+//   darkMode: boolean;
+//   dispatch: React.Dispatch<>;
+// };
 
 export const DarkModeContext = createContext(INITIAL_STATE);
 
 export const DarkModeContextProvider = ({ children }: any) => {
   const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
 
+  // return <DarkModeContext.Provider value={{ darkMode: state.darkMode }}>{children}</DarkModeContext.Provider>;
+  // return <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch }}>{children}</DarkModeContext.Provider>;
   return <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch }}>{children}</DarkModeContext.Provider>;
 };
--- admin_2022/src/store/index.js
+++ admin_2022/src/store/index.js
@@ -1,7 +1,7 @@
-// src/store/index.js
-import { createStore } from 'redux';
-// 用于支持异步action
-// import thunk from "redux-thunk";
-import combineReducers from './reducers/index';
+// // src/store/index.js
+// import { createStore } from 'redux';
+// // 用于支持异步action
+// // import thunk from "redux-thunk";
+// import combineReducers from './reducers/index';
 
-export default createStore(combineReducers, undefined);
+// export default createStore(combineReducers, undefined);
